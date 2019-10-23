634e822a0abf4740b548db95fbcb15cfca08d5b9
tb94 <timbretz94@gmail.com>
2019-10-23 02:40:51 +0000

    Initial Commit

---
+++ gui/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+
+app. use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
